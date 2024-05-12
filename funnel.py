b8db250b5148a3107fa9fe92706cc6d9721889c2
Ray Pomponio <30960863+rpomponio@users.noreply.github.com>
2024-05-12 10:02:12 -0400

    added multipitch filter

--- funnel.py
+++ funnel.py
@@ -38,11 +38,14 @@ app.layout = html.Div([
                  ['Onsight', 'Flash', 'Redpoint'],
                  multi=True, searchable=False, id='send-dropdown',
                  style={'width':'75vw'}),
+    dcc.Checklist(['Include Multipitch Routes'],
+                  ['Include Multipitch Routes'],
+                  id='multi-filter'),
     dcc.Graph(id='graph-pyramid', config=config,
               style={'width':'80vw', 'height':'80vh'})
 ])
 
-def parse_contents(contents, filename, criteria_type, criteria_send):
+def parse_contents(contents, filename, criteria_type, criteria_send, criteria_multi):
     _, content_string = contents.split(',')
 
     # Decode the base64 string & Read CSV
@@ -55,6 +58,8 @@ def parse_contents(contents, filename, criteria_type, criteria_send):
         ticks = ticks.loc[ticks['Lead Style'].isna() | ticks['Lead Style'].isin(criteria_send), :]
     else:
         ticks = ticks.loc[ticks['Lead Style'].isin(criteria_send), :]
+    if criteria_multi == []:
+        ticks = ticks.loc[ticks['Pitches']==1, :]
 
     # Calculate climbing pyramid metrics
     ticks['Grade'] = pd.cut(
@@ -68,16 +73,18 @@ def parse_contents(contents, filename, criteria_type, criteria_send):
     counts.sort_values(by='Grade', ascending=False, inplace=True)
 
     # Create a chart based on the filtered dataframe
-    fig = px.funnel(counts, x='Routes', y='Grade', title='Route Pyramid')
+    fig = px.funnel(counts, x='Routes', y='Grade', title="User's Route Pyramid")
 
     return fig
 
-@app.callback(Output('graph-pyramid', 'figure'),
-              [Input('upload-data', 'contents'),
-               Input('type-dropdown', 'value'),
-               Input('send-dropdown', 'value')],
-              [dash.dependencies.State('upload-data', 'filename')])
-def update_output(contents, criteria_type, criteria_send, filename):
+@app.callback(
+        Output('graph-pyramid', 'figure'),
+        [Input('upload-data', 'contents'),
+         Input('type-dropdown', 'value'),
+         Input('send-dropdown', 'value'),
+         Input('multi-filter', 'value')],
+         [dash.dependencies.State('upload-data', 'filename')])
+def update_output(contents, criteria_type, criteria_send, criteria_multi, filename):
     if contents is None:
         dummy = dict(
             Routes=[1, 0, 1, 0, 2, 3, 5, 8, 16, 32, 64, 80, 70, 90, 100, 120],
@@ -86,7 +93,7 @@ def update_output(contents, criteria_type, criteria_send, filename):
                    "5.10b", "5.10a", "5.9", "5.8", "5.7", "5.easy"])
         return px.funnel(dummy, x='Routes', y='Grade', title='Dummy Data')
     else:
-        fig = parse_contents(contents, filename, criteria_type, criteria_send)
+        fig = parse_contents(contents, filename, criteria_type, criteria_send, criteria_multi)
         return fig
 
 if __name__ == '__main__':
