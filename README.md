b222497415e3a77d844373cc4276d8cc319ce9f7
rpomponio <raymond.pomponio@outlook.com>
2024-11-08 15:28:54 -0500

    deployed shiny app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Climbing Pyramids with R Shiny/Plotly
+
+Here is the link to the web app: <https://rpomponio.shinyapps.io/climbing_viz_app/>
--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 # Load required libraries
 library(shiny)
 library(ggplot2)
+library(plotly)
 library(dplyr)
 library(lubridate)
 library(DT)
@@ -13,20 +14,25 @@ ui <- fluidPage(
     sidebarPanel(
       fileInput("file", "Upload your ticks data (CSV format)",
                 accept = c(".csv")),
+      textInput("user_url", label="Or, copy/paste your Profile URL", value="",
+                placeholder="https://www.mountainproject.com/user/USERNUM/USER-NAME"),
       tags$hr(),
-      checkboxInput("redpoints_only", "Filter to 'sends'", TRUE),
+      checkboxInput("redpoints_only", "Filter to 'Redpoints Only'", TRUE),
+      h6("'Redpoints' include only the following: Flash, Onsight,
+         Redpoint, Pinkpoint"),
       selectInput("date_min", "Select beginning of date range", choices = NULL),
       selectInput("date_max", "Select end of date range", choices = NULL),
       tags$hr(),
-      h5("Instructions: Upload a CSV file containing your climbing data with at
-         least 'Date', 'Lead Style', and 'Rating Code' columns.")
+      h5("Instructions: Upload a CSV file containing your ticks data (exported
+         from MountainProject.com), or paste the URL of your Mountain Project
+         profile page (the page that displays your profile picture).")
     ),
     
     mainPanel(
       tabsetPanel(
         tabPanel("Data Table", DTOutput("table")),
-        tabPanel("Route Pyramid", plotOutput("gradePlot")),
-        tabPanel("Max Grade Over Time", plotOutput("timePlot"))
+        tabPanel("Route Pyramid", plotlyOutput("gradePlot")),
+        tabPanel("Max Grade Over Time", plotlyOutput("timePlot"))
       )
     )
   )
@@ -37,8 +43,14 @@ server <- function(input, output, session) {
   
   # Reactive expression to read the uploaded file
   data <- reactive({
-    req(input$file)
-    raw <- read.csv(input$file$datapath, check.names=FALSE)
+    if (input$user_url != ""){
+      req(input$user_url)
+      url <- gsub("\\/$", "", input$user_url)
+      raw <- read.csv(paste0(url, "/tick-export"), check.names=FALSE)
+    } else {
+      req(input$file)
+      raw <- read.csv(input$file$datapath, check.names=FALSE)
+    }
     clean <- raw %>%
       mutate(YDS=cut(
         `Rating Code`,
@@ -47,7 +59,9 @@ server <- function(input, output, session) {
                  9400, 10400),
         labels=c("5.easy", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d",
                  "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b",
-                 "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.13d"))) %>%
+                 "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.13d"),
+        ordered_result=TRUE)) %>%
+      filter(!is.na(YDS)) %>%
       mutate(Redpoint=case_when(
         `Lead Style` %in% c("Redpoint", "Pinkpoint", "Onsight", "Flash") ~ "Y",
         TRUE ~ "N")) %>%
@@ -73,7 +87,7 @@ server <- function(input, output, session) {
   })
   
   # Render grade distribution plot
-  output$gradePlot <- renderPlot({
+  output$gradePlot <- renderPlotly({
     req(data())
     if (input$redpoints_only){
       df <- filter(data(), Redpoint=="Y")
@@ -81,30 +95,53 @@ server <- function(input, output, session) {
       df <- data()
     }
     df <- df %>%
-      filter(!is.na(YDS)) %>%
       filter(between(Date, ymd(input$date_min), ymd(input$date_max))) %>%
       group_by(YDS) %>%
-      summarise(count=n())
+      summarise(Count=n())
     pyramid.df <- bind_rows(
-      mutate(df, count=count, dummy="A"),
-      mutate(df, count=-count, dummy="B"))
-    count_range <- range(pyramid.df$count)
+      mutate(df, Count=Count, dummy="A"),
+      mutate(df, Count=-Count, dummy="B"))
+    count_range <- range(pyramid.df$Count)
     count_range_seq <- pretty(count_range, n=5)
-    ggplot(pyramid.df, aes(x=count, y=YDS, group=dummy)) +
+    p <- ggplot(pyramid.df, aes(x=Count, y=YDS, group=dummy)) +
       geom_col(fill="lightblue", color="darkblue") +
       labs(title="Route-Climbing Pyramid", x="Routes", y="Grade") +
       scale_x_continuous(breaks=count_range_seq, labels=abs(count_range_seq)) +
       theme_minimal()
+    ggplotly(p, tooltip=c("y", "x"))
   })
   
   # Render climbing frequency over time plot
-  output$timePlot <- renderPlot({
+  output$timePlot <- renderPlotly({
     req(data())
-    df <- data()
-    df$date <- as.Date(df$date)  # Convert to Date format
-    df_summary <- df %>%
-      group_by(date) %>%
-      summarize
+    if (input$redpoints_only){
+      df <- filter(data(), Redpoint=="Y")
+    } else {
+      df <- data()
+    }
+    cummax.df <- df %>%
+      group_by(Date) %>%
+      summarise(day_max=max(`Rating Code`)) %>%
+      arrange(Date) %>%
+      mutate(max_code=cummax(day_max)) %>%
+      mutate(YDS=cut(
+        max_code,
+        breaks=c(0, 1700, 1900, 2200, 2500, 2800, 3100, 3400, 4500, 4800,
+                 5100, 5400, 6500, 6800, 7100, 7400, 8500, 8800, 9100,
+                 9400, 10400),
+        labels=c("5.easy", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d",
+                 "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b",
+                 "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.13d"),
+        ordered_result=TRUE)) %>%
+      mutate(grade_rank=dense_rank(YDS))
+    p <- ggplot(cummax.df, aes(x=Date, y=grade_rank, label=YDS)) +
+      geom_step(color="darkblue", direction="vh") +
+      labs(title="Max Grade Over Time", x="Date", y="Grade") +
+      scale_y_continuous(
+        breaks=cummax.df$grade_rank,
+        labels=cummax.df$YDS) +
+      theme_minimal()
+    ggplotly(p, tooltip=c("x", "label"))
   })
 }
 
