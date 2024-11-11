d6fc90aca87fafe7afa89a76b98c6e7a51d897a1
rpomponio <raymond.pomponio@outlook.com>
2024-11-11 11:06:38 -0500

    app improvements

--- app.R
+++ app.R
@@ -16,23 +16,21 @@ ui <- fluidPage(
                 accept = c(".csv")),
       textInput("user_url", label="Or, copy/paste your Profile URL", value="",
                 placeholder="https://www.mountainproject.com/user/USERNUM/USER-NAME"),
-      tags$hr(),
-      checkboxInput("redpoints_only", "Filter to 'Redpoints Only'", TRUE),
-      h6("'Redpoints' include only the following Lead Styles: Flash, Onsight,
-         Redpoint, Pinkpoint"),
-      selectInput("date_min", "Select beginning of date range", choices = NULL),
-      selectInput("date_max", "Select end of date range", choices = NULL),
-      tags$hr(),
       h5("Instructions: Upload a CSV file containing your ticks data (exported
          from MountainProject.com), or paste the URL of your Mountain Project
-         profile page (the page that displays your profile picture).")
+         profile page (the page that displays your profile picture)."),
+      tags$hr(),
+      checkboxInput("unique", "Filter to unique routes only", value=TRUE),
+      selectInput("date_min", "Select beginning of date range", choices=NULL),
+      selectInput("date_max", "Select end of date range", choices=NULL),
+      tags$hr()
     ),
     
     mainPanel(
       tabsetPanel(
         tabPanel("Data Table", DTOutput("table")),
         tabPanel("Route Pyramid", plotlyOutput("gradePlot")),
-        tabPanel("Max Grade Over Time", plotlyOutput("timePlot"))
+        tabPanel("Route Progression", plotlyOutput("timePlot"))
       )
     )
   )
@@ -46,6 +44,7 @@ server <- function(input, output, session) {
     if (input$user_url != ""){
       req(input$user_url)
       url <- gsub("\\/$", "", input$user_url)
+      url <- gsub("\\/ticks$", "", url)
       raw <- read.csv(paste0(url, "/tick-export"), check.names=FALSE)
     } else {
       req(input$file)
@@ -66,6 +65,16 @@ server <- function(input, output, session) {
         `Lead Style` %in% c("Redpoint", "Pinkpoint", "Onsight", "Flash") ~ "Y",
         TRUE ~ "N")) %>%
       mutate(Date=ymd(Date))
+    if (input$unique){
+      clean <- clean %>%
+        group_by(URL, Route, `Route Type`, `Rating Code`, YDS, Location) %>%
+        arrange(desc(Redpoint), desc(Date), .by_group=TRUE) %>%
+        summarise(
+          Date=first(Date),
+          `Lead Style`=first(`Lead Style`),
+          Redpoint=first(Redpoint)) %>%
+        ungroup()
+    }
     clean
   })
   
@@ -82,6 +91,7 @@ server <- function(input, output, session) {
   output$table <- renderDT({
     req(data())
     df <- data() %>%
+      filter(between(Date, ymd(input$date_min), ymd(input$date_max))) %>%
       select(Date, Route, YDS, `Route Type`, Location, `Lead Style`, Redpoint)
     datatable(df, filter="top")
   })
@@ -89,41 +99,57 @@ server <- function(input, output, session) {
   # Render grade distribution plot
   output$gradePlot <- renderPlotly({
     req(data())
-    if (input$redpoints_only){
-      df <- filter(data(), Redpoint=="Y")
-    } else {
-      df <- data()
-    }
-    df <- df %>%
+    df <- data()
+    pyramid.df <- df %>%
       filter(between(Date, ymd(input$date_min), ymd(input$date_max))) %>%
-      group_by(YDS) %>%
-      summarise(Count=n())
-    pyramid.df <- bind_rows(
-      mutate(df, Count=Count, dummy="A"),
-      mutate(df, Count=-Count, dummy="B"))
-    count_range <- range(pyramid.df$Count)
-    count_range_seq <- pretty(count_range, n=5)
-    p <- ggplot(pyramid.df, aes(x=Count, y=YDS, group=dummy)) +
-      geom_col(fill="lightblue", color="darkblue") +
-      labs(title="Route-Climbing Pyramid", x="Routes", y="Grade") +
-      scale_x_continuous(breaks=count_range_seq, labels=abs(count_range_seq)) +
+      group_by(YDS, Redpoint) %>%
+      summarise(Count=n()) %>%
+      mutate(xcount=case_when(
+        Redpoint=="Y" ~ Count,
+        Redpoint=="N" ~ -Count))
+    count_range <- range(-pyramid.df$xcount[pyramid.df$xcount < 0])
+    count_range_seq <- c(-pretty(count_range, n=5), pretty(count_range, n=5))
+    p <- ggplot(pyramid.df, aes(x=xcount, y=YDS, group=Redpoint, label=Count)) +
+      geom_col(aes(fill=Redpoint, color=Redpoint)) +
+      scale_fill_manual(values=c("lightblue", "pink")) +
+      scale_color_manual(values=c("darkblue", "darkred")) +
+      labs(title="Route Climbing Pyramid", x="Routes", y="Grade") +
+      scale_x_continuous(limits=c(min(pyramid.df$xcount), -min(pyramid.df$xcount)),
+                         breaks=count_range_seq, labels=abs(count_range_seq)) +
       theme_minimal()
-    ggplotly(p, tooltip=c("y", "x")) %>% config(displayModeBar=FALSE)
+    ggplotly(p, tooltip=c("group", "y", "label")) %>%
+      layout(
+        legend=list(
+          itemclick=FALSE,
+          itemdoubleclick=FALSE,
+          groupclick=FALSE),
+        xaxis=list(fixedrange=TRUE),
+        yaxis=list(fixedrange=TRUE)) %>%
+      config(displayModeBar=FALSE)
   })
   
   # Render climbing frequency over time plot
   output$timePlot <- renderPlotly({
     req(data())
-    if (input$redpoints_only){
-      df <- filter(data(), Redpoint=="Y")
-    } else {
-      df <- data()
-    }
+    df <- data() %>%
+      mutate(grade_rank=dense_rank(YDS))
     cummax.df <- df %>%
+      filter(Redpoint == "N") %>%
       group_by(Date) %>%
-      summarise(day_max=max(`Rating Code`)) %>%
+      summarise(day_max=max(`Rating Code`), max_rank=max(grade_rank)) %>%
       arrange(Date) %>%
-      mutate(max_code=cummax(day_max)) %>%
+      mutate(max_code=cummax(day_max), grade_rank=cummax(max_rank)) %>%
+      mutate(Redpoint="N")
+    cummax.df.redpoint <- df %>%
+      filter(Redpoint == "Y") %>%
+      group_by(Date) %>%
+      summarise(day_max=max(`Rating Code`), max_rank=max(grade_rank)) %>%
+      arrange(Date) %>%
+      mutate(max_code=cummax(day_max), grade_rank=cummax(max_rank)) %>%
+      mutate(Redpoint="Y")
+    cummax.df.all <- bind_rows(
+      cummax.df,
+      cummax.df.redpoint) %>%
       mutate(YDS=cut(
         max_code,
         breaks=c(0, 1700, 1900, 2200, 2500, 2800, 3100, 3400, 4500, 4800,
@@ -132,16 +158,24 @@ server <- function(input, output, session) {
         labels=c("5.easy", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d",
                  "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b",
                  "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.13d"),
-        ordered_result=TRUE)) %>%
-      mutate(grade_rank=dense_rank(YDS))
-    p <- ggplot(cummax.df, aes(x=Date, y=grade_rank, label=YDS)) +
-      geom_step(color="darkblue", direction="vh") +
-      labs(title="Max Grade Over Time", x="Date", y="Grade") +
+        ordered_result=TRUE))
+    p <- ggplot(cummax.df.all, aes(x=Date, y=grade_rank, group=Redpoint, label=YDS)) +
+      geom_step(aes(color=Redpoint), direction="vh") +
+      scale_color_manual(values=c("darkblue", "darkred")) +
+      labs(title="Max Grades Over Time", x="Date", y="Grade") +
       scale_y_continuous(
-        breaks=cummax.df$grade_rank,
-        labels=cummax.df$YDS) +
+        breaks=df$grade_rank,
+        labels=df$YDS) +
       theme_minimal()
-    ggplotly(p, tooltip=c("label", "x")) %>% config(displayModeBar=FALSE)
+    ggplotly(p, tooltip=c("group", "label", "x")) %>%
+      layout(
+        legend=list(
+          itemclick=FALSE,
+          itemdoubleclick=FALSE,
+          groupclick=FALSE),
+        xaxis=list(fixedrange=TRUE),
+        yaxis=list(fixedrange=TRUE)) %>%
+      config(displayModeBar=FALSE)
   })
 }
 
